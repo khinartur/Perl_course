0baf8cd5bc7b654e055c6f7679ff5d19aa76eb9e
Artur Khineltsev <khinartur@mail.ru>
2017-02-28 22:22:37 +0300

    Add directory of second hometask

---
+++ Hometask2:santa/lib/SecretSanta.pm
@@ -0,0 +1,34 @@
+package SecretSanta;
+
+use 5.010;
+use strict;
+use warnings;
+use DDP;
+
+sub calculate {
+	my @members = @_;
+	my @res;
+	# ...
+	#	push @res,[ "fromname", "toname" ];
+	# ...
+	
+	
+	foreach my $fromname (@members) {
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+	
+	return @res;
+}
+
+1;
