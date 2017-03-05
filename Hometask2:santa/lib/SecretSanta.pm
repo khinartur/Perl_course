08b974736b8a8bfe79334778ed6cda1787ea2493
Artur Khineltsev <khinartur@mail.ru>
2017-03-05 23:19:07 +0300

    Add first version of hometask 2

--- Hometask2:santa/lib/SecretSanta.pm
+++ Hometask2:santa/lib/SecretSanta.pm
@@ -11,21 +11,35 @@ sub calculate {
 	# ...
 	#	push @res,[ "fromname", "toname" ];
 	# ...
-	
-	
-	foreach my $fromname (@members) {
+	my (@values, %pairs_done, %family); #@values - массив из всех имен без ссылок, %pairs_done - хэш пар, составленных случайным образом,
+										#%family - хэш супружеских пар, которые по условию запрещены в качестве результата
 
+	foreach my $val (@members) {		#формирование массива @values и хэша %family	
+		unless (ref $val) {
+			push @values, $val;
+		} else {
+			my @pair = ($val->[0], $val->[1]);
+			$family{$val->[0]} = $val->[1];
+			$family{$val->[1]} = $val->[0];
+			push @values, @pair;
+		}
 	}
 
+	my %unused_tonames;					#%unused_tonames - имена, которые еще не получили подарки
+	@unused_tonames{@values} = (); 
+	%pairs_done = map {$_ => @values[rand @values]} @values;
 
+	while (my ($fromname, $toname) = each %pairs_done) {	 #проверка правильности подбора пар случайным образом
 
-
-
-
-
-
-
-
+    	while ($fromname eq $toname || $pairs_done{$toname} eq $fromname || 
+    		exists $family{$fromname} && $toname eq $family{$fromname} || !(exists $unused_tonames{$toname})) {
+    		
+    		$toname = @values[rand @values];	#если случайно подобранная пара не удовлетворяет условиям, то подобрать дарителя снова
+    	}
+    	
+    	push @res, [$fromname, $toname];		#поместить сслыку на полученную пару в результирующий массив
+    	delete $unused_tonames{$toname};		#$toname больше не может быть использован в качестве дарителя
+	}
 
 	
 	return @res;
