547512057e0b084792344f9dbadbb183dd3a3ad2
Artur Khineltsev <khinartur@mail.ru>
2017-04-04 14:44:56 +0300

    New version of log-analyser

--- log-analyzer/bin/analyze.pl
+++ log-analyzer/bin/analyze.pl
@@ -4,7 +4,6 @@ use strict;
 use warnings;
 our $VERSION = 1.0;
 
-use POSIX;
 use List::Util qw/min/;
 
 my $filepath = $ARGV[0];
@@ -22,25 +21,21 @@ sub parse_file {
     # you can put your code here ВЫДЕЛЕН ДОБАВЛЕННЫЙ МНОЙ КОД
     #######################################################
 
-    my $query_count;    #количество запросов в общем
-    my %total_minutes;      #все минуты, в которых происходят запросы
-    my $total_data;     #количество несжатых данных всего
-    my %data_of_status;     #статусы запросов и сжатые данные которые были переданы при них
-    my %hash_of_ip;     #хэш всех ip, с которых происходили запросы. Включает необходимые для вывода данные    
+    my %hash_of_ip = (total => {"ip" => "total"});     #хэш всех ip, с которых происходили запросы. Включает необходимые для вывода данные    
     my ($ip, $minute, $status, $compressed_data, $indices);     #информация из строки лога
     my @top_10_ip;     #топ десять ip адресов для вывода в результат
-    my %top10_hash;    #вспомогательный массив для формирования @top_10_ip
+    my %hash_all;      #вспомогательный хэш, хранящий только ip и количество запросов
 
     #######################################################
 
-     my $fd;
+    my $fd;
     if ($file =~ /\.bz2$/) {
         open $fd, "-|", "bunzip2 < $file" or die "Can't open '$file' via bunzip2: $!";
     } else {
         open $fd, "<", $file or die "Can't open '$file': $!";
     }
 
-    my $result;
+    my @result;
     while (my $log_line = <$fd>) {
 
         # you can put your code here
@@ -49,38 +44,23 @@ sub parse_file {
 
         next unless ($ip, $minute, $status, $compressed_data, $indices) = $log_line =~ 
                 /^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) \[(\d{1,2}\/[JFMASOND][a-z]{2}\/\d{4}:\d{2}:\d{2}):\d{2} \+\d{4}\] "[A-Z]+ [^"]+" (\d{3}) (\d{1,}) "[^"]+" "[^"]+" "(.+)"$/;
-        $query_count++;
 
         $indices = 1 if $indices eq "-";
 
-        my $uncompressed_data = $status eq "200" ? floor ($compressed_data * $indices) : 0;    #количество несжатых данных
+        my $uncompressed_data = $status eq "200" ? int ($compressed_data * $indices) : 0;    #количество несжатых данных
 
-        $total_data += $uncompressed_data;           #добавляем к счетчику несжатых данных
-        $total_minutes{$minute} = 1;                    #минута, в которой произошел запрос
-        $data_of_status{$status} += $compressed_data;   #количество сжатых данных по статусам запросов 
+        $hash_of_ip{$ip}{"ip"} = $ip;
+        $hash_of_ip{total}{"data"} += $uncompressed_data;  #добавляем к счетчику несжатых данных   
+        $hash_of_ip{total}{"count"}++;     #добавляемк общему числу запросов            
+        $hash_of_ip{total}{"minutes"}{$minute} = 1; #минута, в которой произошел запрос
+        $hash_of_ip{total}{data_of_status}{$status} += $compressed_data; #количество сжатых данных по статусам запросов
         $hash_of_ip{$ip}{"count"}++;                    #количество запросов от данного ip адреса
         $hash_of_ip{$ip}{"minutes"}{$minute} = 1;       #минуты, в которых происходили запросы от данного ip адреса
         $hash_of_ip{$ip}{"data"} += $uncompressed_data; #количество несжатых данных от данного ip
-        $hash_of_ip{$ip}{$status} += $compressed_data;  #количество сжатых данных по каждому статусу от ip
-
-        my $min_val = min values %top10_hash;
+        $hash_of_ip{$ip}{data_of_status}{$status} += $compressed_data;  #количество сжатых данных по каждому статусу от ip
         
-        if (scalar keys %top10_hash == 10) {            #формируем в процессе хэш 10 ip адресов с наибольшим количеством запросов
-                if ($hash_of_ip{$ip}{"count"} > min values %top10_hash) {
-
-                    $top10_hash{$ip} = $hash_of_ip{$ip}{"count"};
-
-                    if (scalar keys %top10_hash > 10) {
-                        foreach (keys %top10_hash) {
-                            delete $top10_hash{$_} if $top10_hash{$_} == $min_val;
-                        }
-                    } 
-
-                }
-        }
-        else {
-            $top10_hash{$ip} = $hash_of_ip{$ip}{"count"};
-        }
+       delete $hash_all{$ip} if exists $hash_all{$ip};
+       $hash_all{$ip} = $hash_of_ip{$ip}{"count"};
 
     #######################################################
 
@@ -91,26 +71,18 @@ sub parse_file {
     # you can put your code here
     #######################################################
 
-    foreach (sort {$a <=> $b} values %top10_hash) { #сортируем значения хэша топ 10 ip адресов 
-        my $arg = $_;
-        my @a = grep {$top10_hash{$_} == $arg} keys %top10_hash;
-        unshift @top_10_ip, @a;
+    @top_10_ip = sort {$hash_all{$b} <=> $hash_all{$a}} keys %hash_all;
+    @top_10_ip = splice @top_10_ip, 0, 10;
+
+    unshift @top_10_ip, "total";
+
+    foreach (@top_10_ip) {
+        push @result, $hash_of_ip{$_};
     }
-
-    my $count_of_minutes = scalar keys %total_minutes;
-
-    $result = {
-        "hash_of_ip" => \%hash_of_ip,
-        "top_ip" => \@top_10_ip,
-        "Count" => $query_count,
-        "Data" => $total_data,
-        "Status" => \%data_of_status,
-        "Minutes" => $count_of_minutes,
-    }; 
 
     #######################################################
 
-    return $result;
+    return \@result;
 }
 
 sub report {
@@ -120,31 +92,18 @@ sub report {
     #######################################################
     my $kb = 1024;  #для вывода в килобайтах
 
-    print "IP\tcount\tavg\tdata";
-    my @sort_status = sort keys %{$result->{"Status"}};
-    foreach (@sort_status) {
-        print "\t$_";
-    }
-    printf "\n";
-    printf "%s\t%s\t%.2f\t%.0f","total",$result->{"Count"},$result->{"Count"}/$result->{"Minutes"}, floor $result->{"Data"}/1024;
-    foreach (@sort_status) {
-        printf "\t%d", floor $result->{"Status"}{$_}/$kb;
-    }
-    print "\n";
+    my @sort_status = sort keys @{$result}[0]->{data_of_status};
+    $" = "\t";
+    print "IP\tcount\tavg\tdata\t@sort_status\n";
 
-    foreach (@{$result->{'top_ip'}}) {
-        print("$_\t");
-        print("$result->{'hash_of_ip'}{$_}{'count'}\t");
-        printf("%.2f\t%d", $result->{"hash_of_ip"}{$_}{"count"}/(scalar keys %{$result->{"hash_of_ip"}{$_}{"minutes"}}),
-                        floor $result->{"hash_of_ip"}{$_}{"data"}/$kb);
-
+    foreach my $temp_ip (@$result) {
+        
+        printf "%s\t%d\t%.2f\t%d", $temp_ip->{'ip'}, 
+            $temp_ip->{'count'}, $temp_ip->{'count'} / scalar keys $temp_ip->{'minutes'}, $temp_ip->{'data'}/$kb;
         foreach my $status (@sort_status) {
-            printf "\t%d", $result->{"hash_of_ip"}{$_}{$status} ? floor $result->{"hash_of_ip"}{$_}{$status}/$kb : 0;
+            printf "\t%d", $temp_ip->{data_of_status}{$status} ? $temp_ip->{data_of_status}{$status}/$kb : 0;
         }
         print "\n";
     }
-
     #######################################################
-
-
 }
